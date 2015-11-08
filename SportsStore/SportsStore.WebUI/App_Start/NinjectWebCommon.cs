1dc11adf2a8f40119b202f197081cdd5f643c643
Salama Hassona <nirvana007@wp.pl>
2015-11-09 00:25:17 +0100

    SportsStore

--- SportsStore/SportsStore.WebUI/App_Start/NinjectWebCommon.cs
+++ SportsStore/SportsStore.WebUI/App_Start/NinjectWebCommon.cs
@@ -1,3 +1,6 @@
+using System.Web.Mvc;
+using SportsStore.WebUI.Infrastructure;
+
 [assembly: WebActivatorEx.PreApplicationStartMethod(typeof(SportsStore.WebUI.App_Start.NinjectWebCommon), "Start")]
 [assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(SportsStore.WebUI.App_Start.NinjectWebCommon), "Stop")]
 
@@ -61,6 +64,7 @@ namespace SportsStore.WebUI.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            DependencyResolver.SetResolver(new NinjectDependencyResolver(kernel));
         }        
     }
 }
