aa8c1a7e7ff21a43b2f9dfe97f0b1a748466a4e8
Salama Hassona <nirvana007@wp.pl>
2015-12-24 11:42:12 +0100

    unit tests fix

--- SportsStore/SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore/SportsStore.UnitTests/UnitTest1.cs
@@ -92,19 +92,19 @@ namespace SportsStore.UnitTests
                 new Product {ProductID = 1, Name = "P1", Category = "Cat1"},
                 new Product {ProductID = 2, Name = "P2", Category = "Cat2"},
                 new Product {ProductID = 3, Name = "P3", Category = "Cat1"},
-                new Product {ProductID = 4, Name = "P4", Category = "Cat3"},
+                new Product {ProductID = 4, Name = "P4", Category = "Cat2"},
                 new Product {ProductID = 5, Name = "P5", Category = "Cat3"},
             });
 
             ProductController controller = new ProductController(mock.Object);
             controller.PageSize = 3;
 
-            Product[] result = ((ProductsListViewModel)controller.List("Cat2", 2).Model)
+            Product[] result = ((ProductsListViewModel)controller.List("Cat2", 1).Model)
                 .Products.ToArray();
 
             Assert.AreEqual(result.Length, 2);
             Assert.IsTrue(result[0].Name=="P2" && result[0].Category=="Cat2");
-            Assert.IsTrue(result[0].Name=="P4" && result[0].Category=="Cat2");
+            Assert.IsTrue(result[1].Name=="P4" && result[1].Category=="Cat2");
         }
 
         [TestMethod]
