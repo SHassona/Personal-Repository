9bdbc1b992ee3f259ff09cb31b0c7f228d94b249
SHassona <nirvana007@wp.pl>
2016-07-23 01:09:28 +0200

    ninject configuration

---
+++ WebApi2Book/src/WebApi2Book.Web.Common/NinjectDependencyResolver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http.Dependencies;
+using Ninject;
+
+namespace WebApi2Book.Web.Common
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        public NinjectDependencyResolver(IKernel container)
+        {
+            Container = container;
+        }
+        public IKernel Container { get; }
+
+        public object GetService(Type serviceType)
+        {
+            return Container.TryGet(serviceType);
+        }
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return Container.GetAll(serviceType);
+        }
+        public IDependencyScope BeginScope()
+        {
+            return this;
+        }
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
