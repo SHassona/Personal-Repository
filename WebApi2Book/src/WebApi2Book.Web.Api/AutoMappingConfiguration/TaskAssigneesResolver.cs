b9fc0ea2a02ab7157bff5da8be3ce1dfa3da6ea3
SHassona <nirvana007@wp.pl>
2016-08-15 17:15:46 +0200

    Task and User Relationships

--- WebApi2Book/src/WebApi2Book.Web.Api/AutoMappingConfiguration/TaskAssigneesResolver.cs
+++ WebApi2Book/src/WebApi2Book.Web.Api/AutoMappingConfiguration/TaskAssigneesResolver.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
-using WebApi2Book.Common.TypeMapping;
 using WebApi2Book.Data.Entities;
 using WebApi2Book.Web.Common;
 using User = WebApi2Book.Web.Api.Models.User;
 
 namespace WebApi2Book.Web.Api.AutoMappingConfiguration
 {
-    public class TaskAssigneesResolver : IValueResolver<Task, object, List<User>>
+    public class TaskAssigneesResolver : IValueResolver<Task, Models.Task, List<User>>
     {
-        public IAutoMapper AutoMapper => WebContainerManager.Get<IAutoMapper>();
+        public IMapper AutoMapper => WebContainerManager.Get<IMapper>();
+//        private readonly IMapper _autoMapper;
 
+//        public TaskAssigneesResolver(IMapper autoMapper)
+//        {
+//            _autoMapper = autoMapper;
+//        }
         protected  List<User> ResolveCore(Task source)
         {
             return source.Users.Select(x => AutoMapper.Map<User>(x)).ToList();
@@ -21,7 +25,7 @@ namespace WebApi2Book.Web.Api.AutoMappingConfiguration
 //        {
 //            return source.Users.Select(x => AutoMapper.Map<User>(x)).ToList();
 //        }
-        public List<User> Resolve(Task source, object destination, List<User> destMember, ResolutionContext context)
+        public List<User> Resolve(Task source, Models.Task destination, List<User> destMember, ResolutionContext context)
         {
             return source.Users.Select(x => AutoMapper.Map<User>(x)).ToList();
         }
