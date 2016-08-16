dc67457e635461ba68636502abbd03729412d2b5
SHassona <nirvana007@wp.pl>
2016-08-16 07:14:47 +0200

    Partial Update of a Task Using PUT/PATCH

--- WebApi2Book/src/WebApi2Book.Web.Api.Models/Task.cs
+++ WebApi2Book/src/WebApi2Book.Web.Api.Models/Task.cs
@@ -1,23 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApi2Book.Web.Api.Models
 {
     public class Task : ILinkContaining
     {
         private List<Link> _links;
+        private bool _shouldSerializeAssignees;
+
+        [Key]
         public long? TaskId { get; set; }
+        [Editable(true)]
         public string Subject { get; set; }
+        [Editable(true)]
         public DateTime? StartDate { get; set; }
+        [Editable(true)]
         public DateTime? DueDate { get; set; }
+        [Editable(false)]
         public DateTime? CreatedDate { get; set; }
+        [Editable(false)]
         public DateTime? CompletedDate { get; set; }
+        [Editable(false)]
         public Status Status { get; set; }
+        [Editable(false)]
         public List<User> Assignees { get; set; }
+        [Editable(false)]
         public List<Link> Links
         {
             get { return _links ?? (_links = new List<Link>()); }
             set { _links = value; }
+        }
+        public bool ShouldSerializeAssignees()
+        {
+            return _shouldSerializeAssignees;
         }
 
         public void AddLink(Link link)
@@ -25,14 +41,11 @@ namespace WebApi2Book.Web.Api.Models
             Links.Add(link);
         }
 
-        private bool _shouldSerializeAssignees;
         public void SetShouldSerializeAssignees(bool shouldSerialize)
         {
             _shouldSerializeAssignees = shouldSerialize;
         }
-        public bool ShouldSerializeAssignees()
-        {
-            return _shouldSerializeAssignees;
-        }
+
+
     }
 }
